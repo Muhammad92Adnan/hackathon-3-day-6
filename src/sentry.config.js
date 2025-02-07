f3e2a4f9a644ce7d0561f582931c3f9dacc95b87
Muhammad_Adnan <adnan92muhammad@gmail.com>
2025-02-08 00:23:10 +0500

    completed

---
+++ src/sentry.config.js
@@ -0,0 +1,13 @@
+
+const { withSentryConfig } = require("@sentry/nextjs");
+
+const moduleExports = {
+  
+};
+
+
+const SentryWebpackPluginOptions = {
+  silent: true, 
+};
+
+module.exports = withSentryConfig(moduleExports, SentryWebpackPluginOptions);
